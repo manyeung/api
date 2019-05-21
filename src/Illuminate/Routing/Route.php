2bf76989d0e50cb0380ea5c84880f574a470a53e
Man Yeung <man.yeung@ultd.com.hk>
2019-05-21 18:06:08 +0800

    convert response into Dingo response

--- src/Illuminate/Routing/Route.php
+++ src/Illuminate/Routing/Route.php
@@ -3,6 +3,10 @@
 namespace Dingo\Api\Illuminate\Routing;
 
 use Illuminate\Routing\Route as IlluminateRoute;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Collection;
+use Illuminate\Contracts\Pagination\Paginator;
+use Dingo\Api\Http\Response;
 
 class Route extends IlluminateRoute
 {
@@ -15,6 +19,39 @@ class Route extends IlluminateRoute
      */
     public function run()
     {
-        return app('api.transformer')->transform(parent::run());
+        $response = parent::run();
+
+        if ($response instanceof JsonResponse) {
+            return $response;
+        }
+
+        if (is_string($response) || is_numeric($response) || is_bool($response)) {
+            return new JsonResponse($response);
+        }
+
+        if (is_null($response)) {
+            return new JsonResponse(['data' => null]);
+        }
+
+        $factory = app('api.transformer');
+        $binding = null;
+
+        if ($factory->transformableResponse($response)) {
+            $class = $response;
+
+            if ($this->isCollection($response)) {
+                $class = $response->first();
+            }
+
+            $class = get_class($class);
+            $binding = $factory->getTransformerBindings()[$class];
+        }
+
+        return new Response($response, 200, [], $binding);
+    }
+
+    protected function isCollection($instance)
+    {
+        return $instance instanceof Collection || $instance instanceof Paginator;
     }
 }
